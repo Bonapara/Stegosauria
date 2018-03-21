42db710c6c1de0391281f9a320e7b32cb2090ba7
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-03-21 16:13:25 +0100

    artworks navbar V1 done

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -7,6 +7,35 @@ class ArtworksController < ApplicationController
     @artwork = Artwork.find(params[:id])
   end
 
+  def sculptures   # GET /sculptures
+    @artworks = Artwork.where(artwork_domain: "sculpture")
+    # @sculpture = "hey"
+  end
+
+  def paintings     # GET /paintings
+    @artworks = Artwork.where(artwork_domain: "peinture")
+  end
+
+  def photographs   # GET /photographs
+    @artworks = Artwork.where(artwork_domain: "photographie")
+  end
+
+  def drawings      # GET /drawings
+    @artworks = Artwork.where(artwork_domain: "dessin")
+  end
+
+  def ceramics      # GET /ceramics
+    @artworks = Artwork.where(artwork_domain: "céramique")
+  end
+
+  def installations # GET /installations
+    @artworks = Artwork.where(artwork_domain: "installation")
+  end
+
+  def editions      # GET /editions
+    @artworks = Artwork.where(artwork_domain: "edition")
+  end
+
   def new           # GET /artworks/new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :artists, only: [:index, :show]
   resources :artworks, only: [:index, :show]
+  get 'sculptures', to: 'artworks#sculptures'
+  get 'paintings', to: 'artworks#paintings'
+  get 'photographs', to: 'artworks#photographs'
+  get 'drawings', to: 'artworks#drawings'
+  get 'ceramics', to: 'artworks#ceramics'
+  get 'installations', to: 'artworks#installations'
+  get 'editions', to: 'artworks#editions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
