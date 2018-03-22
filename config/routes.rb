5af7efe77bffbd2defbbd02efd2ad995570109cc
Thomas Colas des Francs <tdesfrancs@gmail.com>
2018-03-22 18:51:55 +0100

    cal first version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   resources :artists, only: [:index, :show]
   resources :artworks, only: [:index, :show]
+  resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
