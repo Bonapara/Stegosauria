b061d3b510c2ecb37581edf7dfbdc227fc64f51f
Tanguy Foujols <tanguy.foujols@icloud.com>
2018-03-25 17:54:06 +0200

    Burger animation menu OK with clean code

--- app/javascript/components/mobile_menu.js
+++ app/javascript/components/mobile_menu.js
@@ -1,26 +1,19 @@
 // Elements selection
 const burgerMenuIcon = document.querySelector("#burger-btn");
 const mobileMenu = document.querySelector(".mobile-menu-container");
-const sectionTitle = document.querySelector(".section-title");
-const closeButton = document.querySelector("#close-button");
-const mainContainer = document.querySelector(".main-container");
 const mobileMenuDarker = document.querySelector(".mobile-menu-darker");
 
 // Function to display/delete the mobile menu
 const displayBurgerMenu = () => {
-  // Displaying mobile menu
-  // mobileMenu.classList.toggle("translation");
-  // Replacing burger menu by close button
+  // Animating the burger menu icon
   burgerMenuIcon.classList.toggle("mobile-navigation");
+  // Translating the mobile menu
   mobileMenu.classList.toggle("translation");
+  // Adding opacity to the existing content
   mobileMenuDarker.classList.toggle("add-opacity");
-  // closeButton.classList.toggle("visible");
-  // Removing margin top on the title section
-  // sectionTitle.classList.toggle("title-mobile");
 };
 
-// When a user clicks on the burger menu icon or close button, the burger menu is displaying
+// When a user clicks on the burger menu icon, the burger menu is displaying
 burgerMenuIcon.addEventListener('click', displayBurgerMenu);
-// closeButton.addEventListener('click', displayBurgerMenu);
 
 
