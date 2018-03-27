ba170cf2f101ead195b8b2e967a3864e6eaaa22c
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-03-27 17:22:56 +0200

    card on carousel done + Link to artist ok

--- app/controllers/homep_controller.rb
+++ app/controllers/homep_controller.rb
@@ -2,12 +2,14 @@ class HomepController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     if HomeP.count > 0
-      # PHOTO N°1 Carousel
-      @photo_slide1 = HomeP.last.photo_slide1
-      # PHOTO N°2 Carousel
-      @photo_slide2 = HomeP.last.photo_slide2
-      # PHOTO N°3 Carousel
-      @photo_slide3 = HomeP.last.photo_slide3
+      @homeP = HomeP.last
+      # # PHOTO N°1 Carousel
+      # @photo_slide1 = .last.photo_slide1
+      # # PHOTO N°2 Carousel
+      # @photo_slide2 = HomeP.last.photo_slide2
+      # # PHOTO N°3 Carousel
+      # @photo_slide3 = HomeP.last.photo_slide3
+      @artist = Artist.find(@homeP.artist_id)
     else
       @photo_slide1 = 'http://placehold.it/1200X500'
       @photo_slide2 = 'http://placehold.it/1200X500'
