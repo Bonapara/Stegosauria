24d1c1fa9870dfccbd31ac349c4e97f22569a5a1
Thomas Colas des Francs <tdesfrancs@gmail.com>
2018-04-06 17:58:50 +0200

    new navbar look

--- app/models/home_p.rb
+++ app/models/home_p.rb
@@ -1,4 +1,5 @@
 class HomeP < ApplicationRecord
+  belongs_to :artist
   mount_uploader :photo_slide1, PhotoUploader
   mount_uploader :photo_slide2, PhotoUploader
   mount_uploader :photo_slide3, PhotoUploader
