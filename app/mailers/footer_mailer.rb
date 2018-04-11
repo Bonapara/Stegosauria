4450883afa810d2aa73d5b097cd5134b0acc321c
Thomas Colas des Francs <tdesfrancs@gmail.com>
2018-04-11 09:40:13 +0200

    footer mailer first t

--- app/mailers/footer_mailer.rb
+++ app/mailers/footer_mailer.rb
@@ -1,9 +1,9 @@
 class NewsletterMailer < ActionMailer::Base
+  default from: 'contact@galerieisabellelaverny.com'
   def capart
     mail(
       :subject => 'Hello from Postmark',
       :to  => 'contact@galerieisabellelaverny.com',
-      :from => 'contact@galerieisabellelaverny.com',
       :html_body => '<strong>Hello</strong> dear Postmark user.',
       :track_opens => 'true')
   end
