0b8d8e548104389879b31882b18e6415125bc3f5
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-04-13 11:58:42 +0200

    connections made with expo and galerie

--- app/controllers/showcases_controller.rb
+++ app/controllers/showcases_controller.rb
@@ -1,16 +1,12 @@
 class ShowcasesController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    if ShowCase.count > 0
-      @showcase = ShowCase.last
-      @artist = Artist.find(@showcase.artist_id)
-      @artwork = Artwork.find(@showcase.artwork_id)
-      # @gallery = Gallery.find(@showcase.gallery_id)
-    else
-      @photo_artist = 'http://placehold.it/1200X500'
-      @photo_artwork = 'http://placehold.it/1200X500'
-      # @photo_gallery = 'http://placehold.it/1200X500'
-    end
+    @showcase = ShowCase.last
+    @artist = Artist.find(@showcase.artist_id)
+    @artwork = Artwork.find(@showcase.artwork_id)
+    @expo = Expo.find(@showcase.expo_id)
+    @galerie = Galerie.find(@showcase.galerie_id)
+
     set_meta_tags reverse: false,
                   title: "De l'art à l'art de vivre",
                   description: "Galerie d'art Paris 17",
