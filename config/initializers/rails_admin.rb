74a58f3fe497fa31c98c6664a7f32490ac320b9a
Thomas Colas des Francs <tdesfrancs@gmail.com>
2018-04-16 11:13:28 +0200

    fixed my bugs

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -149,6 +149,12 @@ RailsAdmin.config do |config|
     field :vernissage_date do
       label "Date du vernissage"
     end
+    field :date_from do
+      label "Du"
+    end
+    field :date_to do
+      label "Au"
+    end
     field :vernissage_hours_from do
       label "Heure début vernissage"
     end
