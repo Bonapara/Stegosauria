3422c082b2b83089a54441b633574f592305a64d
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-06-04 11:53:30 +0200

    seo updated

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -6,18 +6,18 @@ class ArtistsController < ApplicationController
     # SEO
     if @artists.count > 0
       set_meta_tags title: "Nos artistes",
-                    description: "Découvrez les artistes de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17",
+                    description: "La galerie Isabelle Laverny propose une sélection d'oeuvres d'art de différents artistes contemporain français et internationaux comme Karin Boinet, Michel Tréhet, Florence Lemiegre, Beatrice Lecomte, Martina Hejmalova, Victoire d'Harcourt, Juliette Lanos Meynial, Christine Miller et Nathalie Leverger.",
+                    keywords: "Karin Boinet peinture, Karin Boinet Architecture, Karin Boinet photo, Karin Boinet œuvres, Karin Boinet dessin, Michel Tréhet photos, Michel Tréhet parisienne, Michel Tréhet plage, Michel Tréhet barbie, Michel Tréhet photo normandie, Michel Tréhet Deauville , Beatrice Lecomte installation, Beatrice Lecomte miniatures, Béatrice Lecomte Atelier d'artiste, Victoire d’Harcourt photo peinture, Victoire d'Harcourt sculpture, Victoire d’Harcourt palais de Tokyo, Victoire d’Harcourt musée, Catherine de Kerhor sculpture bronze, Catherine de Kerhor sculpture lampe, Nathalie Leverger peinture, Nathalie Leverger peinture mer, Nathalie Leverger bretagne, Nathalie Leverger textile, Martina Hejmalova monde miniature, Martina Hejmalova piscine, Martina Hejmalova aquarium, Florence Lemiegre Raku, Florence Lemiegre moon, Florence Lemiegre céramique, Florence Lemiegre culbuto, Lili Mirante bronze, Lili Mirante Sculpture, Lili Mirante femme, Lili Mirante sculpture animalière, Karine Daisay collage, Karine Daisay peinture, Karine Daisay oiseaux, Karine Daisay bretagne, Karine Daisay édition, Juliette Lanos mobile, Valerie Bétoulaud sérigraphie, Artistes contemporain paris, Artistes contemporain paris 17",
                     og: {
                           title: :title,
                           site_name: :site,
                           description: :description,
-                          image: "#{@artists.first.photo}"
+                          image: "#{@artists.first.photo} | galerie Isabelle Laverny, Paris 17"
                         }
     else
       set_meta_tags title: "Nos artistes",
                     description: "Découvrez les artistes de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17",
+                    keywords: "Karin Boinet peinture, Karin Boinet Architecture, Karin Boinet photo, Karin Boinet œuvres, Karin Boinet dessin, Michel Tréhet photos, Michel Tréhet parisienne, Michel Tréhet plage, Michel Tréhet barbie, Michel Tréhet photo normandie, Michel Tréhet Deauville , Beatrice Lecomte installation, Beatrice Lecomte miniatures, Béatrice Lecomte Atelier d'artiste, Victoire d’Harcourt photo peinture, Victoire d'Harcourt sculpture, Victoire d’Harcourt palais de Tokyo, Victoire d’Harcourt musée, Catherine de Kerhor sculpture bronze, Catherine de Kerhor sculpture lampe, Nathalie Leverger peinture, Nathalie Leverger peinture mer, Nathalie Leverger bretagne, Nathalie Leverger textile, Martina Hejmalova monde miniature, Martina Hejmalova piscine, Martina Hejmalova aquarium, Florence Lemiegre Raku, Florence Lemiegre moon, Florence Lemiegre céramique, Florence Lemiegre culbuto, Lili Mirante bronze, Lili Mirante Sculpture, Lili Mirante femme, Lili Mirante sculpture animalière, Karine Daisay collage, Karine Daisay peinture, Karine Daisay oiseaux, Karine Daisay bretagne, Karine Daisay édition, Juliette Lanos mobile, Valerie Bétoulaud sérigraphie, Artistes contemporain paris, Artistes contemporain paris 17",
                     og: {
                           title: :title,
                           site_name: :site,
@@ -38,7 +38,7 @@ class ArtistsController < ApplicationController
                         title: :title,
                         site_name: :site,
                         description: :description,
-                        image: "#{@artist.photo}"
+                        image: "#{@artist.photo} | galerie Isabelle Laverny, Paris 17"
                       }
   end
 
--- app/controllers/expos_controller.rb
+++ app/controllers/expos_controller.rb
@@ -6,18 +6,18 @@ class ExposController < ApplicationController
     @expos = Expo.where(['date_from < ? AND date_to > ?', Time.now, Time.now])
     if @expos.count > 0
       set_meta_tags title: "#{@expos.first.expo_type} #{l(@expos.first.date_from, format: '%B')} Paris 17",
-                    description: "Découvrez les expositions en cours de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17, expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
                           description: :description,
-                          image: "#{@expos.first.photo}"
+                          image: "#{@expos.first.photo} |  galerie isabelle laverny, Paris 17"
                         }
     else
       set_meta_tags title: "Nos expositions en cours",
-                    description: "Découvrez les expositions en cours de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17, expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
@@ -31,18 +31,18 @@ class ExposController < ApplicationController
     @expos = Expo.where( "date_from > ?", Time.now )
     if @expos.count > 0
       set_meta_tags title: "#{@expos.first.expo_type} #{l(@expos.first.date_from, format: '%B')} Paris 17",
-                    description: "Découvrez les expositions futures de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17, expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
                           description: :description,
-                          image: "#{@expos.first.photo}"
+                          image: "#{@expos.first.photo} |  galerie isabelle laverny, Paris 17"
                         }
     else
       set_meta_tags title: "Nos expositions futures",
-                    description: "Découvrez les expositions futures de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17, expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
@@ -60,18 +60,18 @@ class ExposController < ApplicationController
     @years = years.uniq.sort
     if @expos.count > 0
       set_meta_tags title: "Nos expositions passées",
-                    description: "Découvrez les expositions passées de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17,  expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
                           description: :description,
-                          image: "#{@expos.first.photo}"
+                          image: "#{@expos.first.photo} |  galerie isabelle laverny, Paris 17"
                         }
     else
       set_meta_tags title: "Nos expositions passées",
-                    description: "Découvrez les expositions passées de la Galerie Isabelle Laverny",
-                    keywords: "art, peinture, galerie, Paris 17,  expositions, exposition, exposition paris 17, exposition paris 17ème, exposition galerie isabelle laverny",
+                    description: "La galerie Isabelle Laverny propose des expositions permanentes et temporaires entre ses murs. Elle participe également à des foires en France et à l'étranger",
+                    keywords: "exposition temporaire paris 17, exposition permanentes paris 17, vernissage paris ternes , Vernissage paris Pereire, Vernissage paris porte Maillot, Exposition paris Pereire, Vernissage paris 17, exposition galerie peinture paris, exposition collective galerie paris , Exposition photo paris, exposition paris 17, exposition peinture, exposition art, exposition artistes contemporains",
                     og: {
                           title: :title,
                           site_name: :site,
