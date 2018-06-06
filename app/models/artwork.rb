391e147214f8e86e0358ebd7f7e5c4d6c1ef699d
Thomas Colas des Francs <tdesfrancs@gmail.com>
2018-06-06 13:43:52 +0200

    debug heroku

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,8 @@
 class Artwork < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   mount_uploader :photo_2, PhotoUploader
-  # mount_uploader :photo_3, PhotoUploader
-  # mount_uploader :photo_4, PhotoUploader
+  mount_uploader :photo_3, PhotoUploader
+  mount_uploader :photo_4, PhotoUploader
 
   belongs_to :artist
   has_many :show_cases
